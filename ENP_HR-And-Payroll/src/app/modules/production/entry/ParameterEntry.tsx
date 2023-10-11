cdcc2b7523c69e88a9ab1a129171c11f6ab5d67b
Ahercode <philipkelly407@gmail.com>
2023-10-11 20:08:34 +0000

    parameter entry completed

--- ENP_HR-And-Payroll/src/app/modules/production/entry/ParameterEntry.tsx
+++ ENP_HR-And-Payroll/src/app/modules/production/entry/ParameterEntry.tsx
@@ -6,15 +6,24 @@ import { Api_Endpoint, deleteItem, fetchAppraisals, fetchDocument, postItem, upd
 import { useMutation, useQuery, useQueryClient } from 'react-query'
 import { set, useForm } from 'react-hook-form'
 import { Link, useNavigate, useParams } from 'react-router-dom'
+import { get, map } from 'jquery'
+import { useAuth } from '../../auth'
+import { all } from '@devexpress/analytics-core/analytics-elements-metadata'
 
 const ParameterEntry = () => {
   const [submitLoading, setSubmitLoading] = useState(false)
   const { register, reset, handleSubmit } = useForm()
   const param: any = useParams();
   const tenantId = localStorage.getItem('tenant')
+  const { currentUser } = useAuth()
   let [appraisalName, setAppraisalName] = useState<any>("")
+const [objectiveStatus, setObjectiveStatus] = useState<any>("")
+
+const [deliverableStatus, setDeliverableStatus] = useState<any>("")
+  const { data: appraisalObjectives} = useQuery('appraisalObjectives', () => fetchDocument('AppraisalObjective'), { cacheTime: 5000 })
   const { data: allParameters, isLoading: loading } = useQuery('parameters', () => fetchDocument(`Parameters`), { cacheTime: 5000 })
   const { data: allAppraisals } = useQuery('appraisals', () => fetchDocument('appraisals'), { cacheTime: 5000 })
+  const { data: allObjectiveDeliverables } = useQuery('appraisalDeliverables', () => fetchDocument('AppraisalDeliverable'), { cacheTime: 5000 })
 
 
   const columns: any = [
@@ -22,6 +31,7 @@ const ParameterEntry = () => {
     {
       title: 'Code',
       dataIndex: 'code',
+      key:"code",
       sorter: (a: any, b: any) => {
         if (a.code > b.code) {
           return 1
@@ -35,6 +45,7 @@ const ParameterEntry = () => {
     {
       title: 'Main Parameter',
       dataIndex: 'name',
+      key:"name",
       sorter: (a: any, b: any) => {
         if (a.name > b.name) {
           return 1
@@ -48,6 +59,7 @@ const ParameterEntry = () => {
     {
       title: 'Weight per parameter (%)',
       dataIndex: 'weight',
+      key:"weight",
       sorter: (a: any, b: any) => {
         if (a.status > b.status) {
           return 1
@@ -82,6 +94,110 @@ const ParameterEntry = () => {
     return appraisal.id === 12
   })
 
+  // add a key to dataByID
+  const dataByIDWithKey = dataByID?.map((item: any) => {
+    return { ...item, key: item.id, obj: "Objectives", del: "Deliverables" }
+  })
+
+  const weightSum = (id: any) => {
+
+    return appraisalObjectives?.data.filter((item: any) => item.parameterId === id && item.employeeId === currentUser?.id)
+      .map((item: any) => item.weight)
+      .reduce((a: any, b: any) => parseInt(a) + parseInt(b), 0)
+  };
+
+  // get weight of deliverables from each objective
+  const weightSumDeliverables = (id: any) => {
+    const allObj= appraisalObjectives?.data.filter((item: any) => item.parameterId === id && item.employeeId === currentUser?.id)
+  
+    // console.log("allObjFirst",allObj)
+    // get all deliverables for each objective
+    const allDeliverables = allObj?.map((item: any) => {
+      return allObjectiveDeliverables?.data.filter((del: any) => del.objectiveId === item.id)
+    })
+
+    // console.log("allDeliverables",allDeliverables)
+
+    // return  deliverys whose sum of weight is !== 100
+    const incompleteDeliverables = allDeliverables?.filter((item: any) => {
+      return item?.map((del: any) => parseInt(del.subWeight))
+      .reduce((a: any, b: any) => parseInt(a) + parseInt(b), 0) !== 100
+    })
+
+    return incompleteDeliverables?.length
+  };
+
+  const weightSumStatus = (id: any) => {
+
+    console.log("para",id)
+
+    
+    const allObj = appraisalObjectives?.data.filter((item: any) => item.parameterId === id && item.employeeId === currentUser?.id)
+
+    // return objectives whose sum of weight is !== para.weight
+
+    const allDeliverables = allObj?.map((item: any) => {
+      return appraisalObjectives?.data.filter((del: any) => del.id === item.id)
+    })
+    // const incompleteObjectives = allObj?.map((item: any) => {
+    //   return item?.map((del: any) => parseInt(del.weight))
+    //   .reduce((a: any, b: any) => parseInt(a) + parseInt(b), 0) !== 80
+    // })
+
+    // console.log("incompleteObjectives",incompleteObjectives)
+
+  };
+
+  const getDeliverableStatus = (  )=>{
+    const getOnlyparameters = allParameters?.data?.filter((item: any) => {
+      return item.appraisalId === 12
+    }
+    )
+    const statusAll = getOnlyparameters?.map((item: any) => {
+      return weightSumDeliverables(item.id)
+    })
+
+    setDeliverableStatus(statusAll)
+    // console.log("deliverableStatus",statusAll)
+  }
+
+
+  const getObjectiveStatus = (  )=>{
+
+    const getOnlyparameters = allParameters?.data?.filter((item: any) => {
+      return item.appraisalId === 12
+    }
+  )   
+
+
+    const allDeliverables = getOnlyparameters?.map((pare: any) => {
+      return appraisalObjectives?.data.filter((item: any) => item.parameterId === pare.id && item.employeeId === currentUser?.id)
+    })
+    console.log("allDeliverables",allDeliverables)
+
+    
+  
+  }
+
+useEffect(() => {
+  getObjectiveStatus()
+  getDeliverableStatus()
+}
+, [allParameters])
+  
+
+const OnSubmit = handleSubmit(async (values) => {
+
+  
+  if(deliverableStatus.every((item:any) => item === 0)){
+    message.success("You have successfully submitted your appraisal")
+  }else{
+    return message.error("You have some incomplete deliverables")
+  }
+
+
+
+})
 
   return (
     <div
@@ -96,7 +212,7 @@ const ParameterEntry = () => {
         <div className='table-responsive'>
           <div className='d-flex flex-direction-row justify-content-between align-items-center align-content-center py-4'>
             <div className='text-primary fs-2 fw-bold mb-4'>{`${appraisalData?.name}`}</div>
-            <Button type='primary' disabled={true} size='large'>
+            <Button onClick={OnSubmit} type='primary' size='large'>
               Submit
             </Button>
           </div> 
@@ -104,19 +220,44 @@ const ParameterEntry = () => {
             loading ? <Skeleton active /> :
               <Table
                 columns={columns}
-                dataSource={dataByID}
+                dataSource={dataByIDWithKey}
                 expandable={{
-                  rowExpandable: (record) => record,
+                  // rowExpandable: (record1) => record1,
                   expandedRowRender: (record) =>
-                    <div key={record}>
-                      <div key={dataByID?.id} className='d-flex flex-direction-row align-items-center align-content-center py-4'>
-                        <div className='d-flex flex-direction-row me-4'>
-                          <span className=''>Objectives</span>
-                          <Tag color='red' className='mx-2'>Incomplete</Tag>
+                    <div key={record?.id}>
+                      <div className='d-flex flex-direction-row align-items-center align-content-center py-4'>
+                        <div style={{borderRight:"1px solid rgba(0,0,0,0.4)", paddingRight:"50px"}} className='me-4'>
+                          
+                          <span >{record?.obj}</span>
+                          {
+                            weightSum(record.id) === record?.weight ?
+                              <Tag color='green' className='mx-2' style={{marginBottom:"10px"}}>Complete </Tag> :
+                              <Tag color='red' className='mx-2' style={{marginBottom:"10px"}}>Incomplete </Tag>
+                          }
+                          <br></br>
+                          
+                            <p>
+                              <span >You have done <span className='fw-bold mx-2' style={{color:"Highlight"}}>{`${weightSum(record.id)==='undefined'?0:weightSum(record.id)}%`}</span> of </span>
+                              <span className='fw-bold mx-2'>{`${record?.weight}%`}</span>
+                            </p>
+                          
                         </div>
-                        <div className='d-flex flex-direction-row'>
-                          <span className=''>Deliverables</span>
-                          <Tag color='red' className='mx-2'>Incomplete</Tag>
+                        <div style={{paddingLeft:"50px"}} >
+                          <span >{record?.del}</span>
+                          {
+                            weightSumDeliverables(record.id) !== 0 ?
+                            <>
+                            
+                            <Tag color='red' className='mx-2' style={{marginBottom:"10px"}}>Incomplete </Tag>
+                            <p>
+                              <span >You have some incompleteed deliverables here.</span>
+                              
+                            </p>
+                            
+                            </>:
+                            <Tag color='green' className='mx-2' style={{marginBottom:"10px"}}>Complete </Tag>
+                              
+                          }
                         </div>
                       </div>
                     </div>
